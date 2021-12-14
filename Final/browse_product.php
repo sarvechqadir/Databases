e8ad1547d0bf27499935bfbb8cf6de2a84774516
shezanaveed <77901748+shezanaveed@users.noreply.github.com>
2021-12-14 23:10:22 +0500

    Add files via upload

---
+++ Final/browse_product.php
@@ -0,0 +1,273 @@
+<?php
+    session_start();
+    if(isset($_POST['button'])){
+        if(isset($_SESSION['cart'])){
+            
+            $count = count($_SESSION['cart']);
+            $boo=0;
+            
+            foreach($_SESSION['cart'] as $value){
+                 if($value==$_POST['id'])
+                 {
+                     $boo = 1;
+                     break;
+                 }
+            }
+            if($boo==1)
+            {
+                ?>
+                    <script>
+                        alert("Item already added")
+                        windows.location="browse_product.php"
+                    </script>
+                    <?php
+            }
+            else
+            {
+                $_SESSION['cart'][$count] = $_POST['id'];
+            }
+            
+        }
+        else{
+            $_SESSION['cart'][0] = $_POST['id'];
+        }
+        
+    }
+        
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Theme Made By www.w3schools.com - No Copyright -->
+    <title>Bootstrap Theme The Band</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Lato"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Montserrat"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+      body {
+        font: 400 15px/1.8 Lato, sans-serif;
+        color: #777;
+      }
+      h3,
+      h4 {
+        margin: 10px 0 30px 0;
+        letter-spacing: 10px;
+        font-size: 20px;
+        color: #111;
+      }
+      .container {
+        padding: 80px 120px;
+      }
+      .person {
+        border: 10px solid transparent;
+        margin-bottom: 25px;
+        width: 80%;
+        height: 80%;
+        opacity: 0.7;
+      }
+      .person:hover {
+        border-color: #f1f1f1;
+      }
+      .carousel-inner img {
+        -webkit-filter: grayscale(90%);
+        filter: grayscale(90%); /* make all photos black and white */
+        width: 100%; /* Set width to 100% */
+        margin: auto;
+      }
+      .carousel-caption h3 {
+        color: #fff !important;
+      }
+      @media (max-width: 600px) {
+        .carousel-caption {
+          display: none; /* Hide the carousel text when the screen is less than 600 pixels wide */
+        }
+      }
+      .bg-1 {
+        background: #2d2d30;
+        color: #bdbdbd;
+      }
+      .bg-1 h3 {
+        color: #fff;
+      }
+      .bg-1 p {
+        font-style: italic;
+      }
+      .list-group-item:first-child {
+        border-top-right-radius: 0;
+        border-top-left-radius: 0;
+      }
+      .list-group-item:last-child {
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+      .thumbnail {
+        padding: 0 0 15px 0;
+        border: none;
+        border-radius: 0;
+      }
+      .thumbnail p {
+        margin-top: 15px;
+        color: #555;
+      }
+      .btn {
+        padding: 10px 20px;
+        background-color: #333;
+        color: #f1f1f1;
+        border-radius: 0;
+        transition: 0.2s;
+      }
+      .btn:hover,
+      .btn:focus {
+        border: 1px solid #333;
+        background-color: #fff;
+        color: #000;
+      }
+      .modal-header,
+      h4,
+      .close {
+        background-color: #333;
+        color: #fff !important;
+        text-align: center;
+        font-size: 30px;
+      }
+      .modal-header,
+      .modal-body {
+        padding: 40px 50px;
+      }
+      .nav-tabs li a {
+        color: #777;
+      }
+      #googleMap {
+        width: 100%;
+        height: 400px;
+        -webkit-filter: grayscale(100%);
+        filter: grayscale(100%);
+      }
+      .navbar {
+        font-family: Montserrat, sans-serif;
+        margin-bottom: 0;
+        background-color: #2d2d30;
+        border: 0;
+        font-size: 11px !important;
+        letter-spacing: 4px;
+        opacity: 0.9;
+      }
+      .navbar li a,
+      .navbar .navbar-brand {
+        color: #d5d5d5 !important;
+      }
+      .navbar-nav li a:hover {
+        color: #fff !important;
+      }
+      .navbar-nav li.active a {
+        color: #fff !important;
+        background-color: #29292c !important;
+      }
+      .navbar-default .navbar-toggle {
+        border-color: transparent;
+      }
+      .open .dropdown-toggle {
+        color: #fff;
+        background-color: #555 !important;
+      }
+      .dropdown-menu li a {
+        color: #000 !important;
+      }
+      .dropdown-menu li a:hover {
+        background-color: red !important;
+      }
+      footer {
+        background-color: #2d2d30;
+        color: #f5f5f5;
+        padding: 32px;
+      }
+      footer a {
+        color: #f5f5f5;
+      }
+      footer a:hover {
+        color: #777;
+        text-decoration: none;
+      }
+      .form-control {
+        border-radius: 0;
+      }
+      textarea {
+        resize: none;
+      }
+    </style>
+  </head>
+<body>
+    <button onclick="document.location='shopping_cart.php'">Shopping Cart</button>
+<div class="container-fluid">
+    <div class="row">
+        <h2 style="text-align: center; "> Browse Product </h2>
+    </div>
+<hr style="border-width: 2px;">
+</div>
+</body>
+ <table class="table" style = "text-align: center; ">
+<thead class="thead-dark">
+    <tr>
+        <th> Product Name</th>
+        <th> Seller </th>
+        <th> Description </th>
+        <th> Price </th>
+        <th> Status </th>
+        
+    </tr>
+</thead>
+<?php 
+ $conn = new mysqli('localhost', 'root', '', 'db_22');
+    if($conn->connect_error){
+        die('Connection Failed : ' .$conn->connect_error);
+    }
+    $stmt = "SELECT * FROM products";
+
+    $result = mysqli_query($conn, $stmt);
+while($data = mysqli_fetch_array($result))
+{
+     $filepath = "images/" .$data['img'];
+?>
+    <tr>
+        <td><?php echo $data['product_name']; ?></td>
+        <td><?php echo $data['username_seller']; ?></td>
+        <td><?php echo $data['descr']; ?></td>
+        <td><?php echo $data['price']; ?></td>
+        <td><?php echo $data['stat']; ?></td>
+        <td><?php echo "<img src=".$filepath." height=200 width=300 />"; ?></td>
+        <td>
+            <form method="post">
+                <input type="hidden" name="id" value="<?php echo $data['product_id'];?>"/>
+                <input type="submit" name="button" value="Add to Cart"/>
+            </form>
+            
+                        
+        </td>
+        
+        
+    </tr>
+    <?php
+}
+        
+?>
+</table>
+
+</body>
+</html>
